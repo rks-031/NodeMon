a4cb19e6958fe259810ed970a529785a68867f7c
Rajnish Kumar Singh <rajnish31july@gmail.com>
2024-11-10 01:56:55 +0530

    name and placeholder altered

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -33,13 +33,13 @@ const Dashboard = () => {
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-dark text-white">
-      <h1 style={{ marginBottom: '20px' }}>AyushFitness</h1>
+      <h1 style={{ marginBottom: '20px' }}>Listify Social</h1>
       <div className="border rounded p-4" style={{ width: '300px', textAlign: 'center' }}>
         <div className="input-group">
           <input
             type="text"
             className="form-control"
-            placeholder="Connect to Insta"
+            placeholder="Connect your social handle"
             value={instaUrl}
             onChange={handleInputChange}
           />
